9341375d7057cf4fe6706759d690ff1758b553b8
Shaik Jani Shaida <96036214+jani474747@users.noreply.github.com>
2022-11-11 12:39:32 +0530

    updated status

--- src/components/Sheet3.js
+++ src/components/Sheet3.js
@@ -1,5 +1,5 @@
-import {useState} from 'react'
-import {Data} from '../components/Data'
+import { useState } from 'react'
+import { Data } from '../components/Data'
 import * as XLSX from 'xlsx'
 import { Box, Button, Grid, Typography } from '@mui/material';
 import { ToastContainer, toast } from 'react-toastify';
@@ -7,140 +7,153 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 function SheetData() {
+
+  
+const [errorMessage, setErrorMessage]=useState([]);
   let i = 0;
 
-  const [excelFile, setExcelFile]=useState(null);
- // const [excelFileError, setExcelFileError]=useState(null);  
- const [error,setError] = useState('Yes')
- 
+  const [excelFile, setExcelFile] = useState(null);
 
-  const [excelData, setExcelData]=useState(null);
 
-  //const fileType=['application/vnd.ms-excel'];
-  const handleFile = (e)=>{
+
+  const [excelData, setExcelData] = useState(null);
+
+
+  const handleFile = (e) => {
     let selectedFile = e.target.files[0];
-    // if(selectedFile){
-     //alert('send me details',selectedFile)
-     
-      if(selectedFile){
-        //console.log(selectedFile)
-        let reader = new FileReader();
-        reader.readAsArrayBuffer(selectedFile);
-        reader.onload=(e)=>{
-          //setExcelFileError(null);
-          setExcelFile(e.target.result);
-        } 
-       //alert('send me details')
+
+    if (selectedFile) {
+
+      let reader = new FileReader();
+      reader.readAsArrayBuffer(selectedFile);
+      reader.onload = (e) => {
+
+        setExcelFile(e.target.result);
       }
-      // else{
-        //setExcelFileError('Please select only excel file types');
-      
-      // }
-    // }
-    // else{
-    //   console.log('plz select your file');
-    // }
+
+    }
+
   }
 
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(excelFile!==null){
-      const workbook = XLSX.read(excelFile,{type:'buffer'});
+    if (excelFile !== null) {
+      const workbook = XLSX.read(excelFile, { type: 'buffer' });
       const worksheetName = workbook.SheetNames[0];
-      const worksheet=workbook.Sheets[worksheetName];
+      const worksheet = workbook.Sheets[worksheetName];
       const data = XLSX.utils.sheet_to_json(worksheet);
-      //console.log('Data-->',data[0].Name)
-      
-      // const handleChanges=()=>{
-        
-        
-        for(let i=0;i<data.length;i++){
-          // setError(data[i])
-          // console.log(data[i])
-          if(!data[i].Name || !data[i].Email || !data[i].Mobile || !data[i].Address || !data[i].Country){
 
-            data[i].Status = 'Data Missing'
-            data[i].Error = 'Error occurs'
-          }else{
-            data[i].Status = 'Success'
-            data[i].Error = 'No Error'
-          }
-          //return data[i]
+
+
+      for (let i = 0; i < data.length; i++) {
+
+        // if (!data[i].Name || !data[i].Email || !data[i].Mobile || !data[i].Address || !data[i].Country) {
+          
+        //   console.log(data[i])
+        //   data[i].Status = 'Data Missing' 
+        //   data[i].Error = 'Error occurs'
+        // } else {
+        //   data[i].Status = 'Success'
+        //   data[i].Error = 'No Error'
+        //   console.log("dataaaaaa->",data[i]);
+        // }
+        
+        if(!data[i].Name){
+          //let Status = 
+          setErrorMessage(errorMessage => errorMessage + 'Name is missing')
+          data[i].Status = errorMessage;
+          //console.log('Data---->',errorMessage)
+         
+           
         }
-        setExcelData(data);
-      // }
+        
+        if(!data[i].Email){
+          //console.log("email", errorMessage)
+          setErrorMessage(errorMessage => errorMessage + "Email is missing")
+          data[i].Status = errorMessage;
+         
+        }
+        if(!data[i].Mobile){
+          setErrorMessage(errorMessage => errorMessage + "Mobile is missing")
+          data[i].Status = errorMessage;
+          
+        }
+        if(!data[i].Address){
+          setErrorMessage(errorMessage => errorMessage + "Address is missing")
+          data[i].Status = errorMessage;
+        
+
+        }
+        if(!data[i].Country){
+          setErrorMessage(errorMessage => errorMessage + "Counrty is missing")
+          data[i].Status = errorMessage;
+         
+
+        }
+       
+    
+
+      }
+      setExcelData(data);
 
     }
-    else{
+    else {
       setExcelData(null);
       toast.error('File not Found !', {
         position: toast.POSITION.TOP_CENTER
-    });
+      });
     }
 
 
 
 
   }
-  
+
   return (
-    
+
     <Box>
 
       <Grid >
         <form onSubmit={handleSubmit}>
           <Typography>Upload Excel file</Typography>
-         
+
           <input type='file' className='form-control'
-          onChange={handleFile} 
-          // required
-           />                  
-          {/* {excelFileError&&<div className='text-danger'
-          style={{marginTop:5+'px'}}>{excelFileError}</div>} */}
+            onChange={handleFile} />
+
           <Button type='submit' className='btn btn-success'
-          style={{marginTop:5+'px'}}>Submit</Button>
+            style={{ marginTop: 5 + 'px' }}>Submit</Button>
         </form>
       </Grid>
 
-   
+
       <hr></hr>
 
       <div className='viewer'>
-        {excelData===null&&<>No file selected</>}
-        {excelData!==null&&(
+        {excelData === null && <>No file selected</>}
+        {excelData !== null && (
           <div>
-            <table style = {{padding : '2% 5% 0% 10%'}}>
+            <table style={{ padding: '2% 5% 0% 10%' }}>
               <thead >
                 <tr>
-                  {/* <th scope='col'>ID</th> */}
-                  <th style={{paddingRight : '1%'}} scope='col'>Name</th>
+
+                  <th style={{ paddingRight: '1%' }} scope='col'>Name</th>
                   <th scope='col'>Email</th>
                   <th scope='col'>Mobile</th>
                   <th scope='col'>Address</th>
                   <th scope='col'>Country</th>
                   <th scope='col'>Status</th>
-                  <th scope = 'col'>Error</th>
-                  {/* <th scope='col'>{status.map((item)=>{
-                  })}</th> */}
-                   {/* { 
-                Name == '' && Email == '' && Mobile == '' && Address == '' && Country == ''  ? null :  
+                  <th scope='col'>Error</th>
 
-                <th scope='col'>error</th>
-                   }           */}
-                {/* { 
-                row.pickup_date == null ? '' :  <TableCell component="th" scope="row">
-                  {row.courier_name}
-                </TableCell>} */}
                 </tr>
               </thead>
               <tbody>
                 <Data excelData={excelData}
                 />
               </tbody>
-            </table>            
+            </table>
           </div>
-        )}       
+        )}
       </div>
       <ToastContainer />
 
