abb94878b0607dc1fb07d10e7b6e000b7cb11f90
Shaik Jani Shaida <96036214+jani474747@users.noreply.github.com>
2022-11-22 14:47:39 +0530

    done with bottom showing data

--- src/components/Sheet3.js
+++ src/components/Sheet3.js
@@ -4,22 +4,16 @@ import { Typography } from '@mui/material';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import React, { useEffect, useMemo, useState } from 'react';
-// import UsePagination from '../components/UsePagination';
-// import Pagenation from './TableData';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Pagination from './Pagenation';
-// import axios from 'axios';
-// import BootstrapTable from 'react-bootstrap-table-next';
-// import paginationFactory from 'react-bootstrap-table2-paginator';
-// import * as ReactBootStrap from 'react-bootstrap'
-// import { CustomTablePagination } from './customtable'
+import { CustomTablePagination } from './customtable'
 import { useRef } from 'react'
-// import "bootstrap/dist/css/bootstrap.min.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.css";
 import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
 import ReactPaginate from "react-paginate";
 import '../Pagenation.css'
-
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+// import TablePagination from "@material-ui/core/TablePagination";
+// import ReactTable from 'react-table'
 
 
 
@@ -40,6 +34,8 @@ function SheetData() {
     const value = "20";
     return value;
   };
+
+
   //const refresh = useRef(null)
   const inputRef = useRef(null);
 
@@ -53,39 +49,45 @@ function SheetData() {
   const [loading, setLoading] = useState(false);
   const [player, setPlayer] = useState([])
   const [order, setOrder] = useState('ASC');
-  // const [page, setPage] = useState(0);
-  // const [rowsPerPage, setRowsPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
   const [Perpage, setPerpage] = useState()
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState();
   const [rowsPerPage, setRowsPerPage] = useState(10);
   // const [user, setUser] = useState(excelData.slice(0, 50))
   const [pageNumber, setPageNumber] = useState(0);
+  // const [showres, setShowres] = useState(10)
+  const [pageSize, setPageSize] = useState(10);
+
 
 
   const usersPerPage = 10;
   const pagesVisited = pageNumber * usersPerPage;
 
+  //Rows per page
+
+
+  // const { data } = useDemoData({
+  //   dataSet: 'Commodity',
+  //   rowLength: 100,
+  //   maxColumns: 6,
+  // });
+
+  // const handlePageSizeChange = (params) => {
+  //   setPageSize(params.pageSize);
+  // };
  
+const PerPage = page
+
+
   const pageCount = excelData ? Math.ceil(excelData.length / usersPerPage) : 0;
-  
-  const pagehandler = excelData ? Math.ceil( 1 - pageCount + excelData.length -1) : 0;
+
+  const pagehandler = excelData ? Math.ceil(1 - {PerPage} + excelData.length - 1) : 0;
 
   const changePage = ({ selected }) => {
     setPageNumber(selected);
+    
   };
 
-
-  // const [postsPerPage, setPostsPerPage] = useState(8);
-
-  // const Pages = ({ excelData }) => {
-  //   const [
-  //     totalPages,
-  //     startPageIndex,
-  //     endPageIndex,
-  //     currentPageIndex, //eslint-disable-line
-  //     displayPage,
-  //   ] = usePagination(5, excelData.length);
 
   const handleFile = (e) => {
     let selectedFile = e.target.files[0];
@@ -107,6 +109,12 @@ function SheetData() {
   const handleClick = () => {
     // setDisable(false);
   };
+
+
+  // const handleChangeRowsPerPage = (event) => {
+  //   setRowsPerPage(parseInt(event.target.value, 10));
+  //   setPage(0);
+  // };
 
 
   const handleSubmit = (e) => {
@@ -144,8 +152,8 @@ function SheetData() {
         }
 
 
-        
-        
+
+
       }
       console.log(data)
       // console.log(data.slice(0,50))
@@ -153,9 +161,9 @@ function SheetData() {
       //setPerpage(data.slice(0,50))
       setSearch(data);
       // pagenationFunc()
-      // setExcelData(data.slice(0, 500));
+      setExcelData(data.slice(0, 500));
       //setDisable(true);
-    
+
 
     }
     else {
@@ -211,29 +219,6 @@ function SheetData() {
 
   }
 
-  // const emptyRows =
-  //   page > 0 ? Math.max(0, (1 + page) * rowsPerPage - excelData.length) : 0;
-
-
-  //   const handleChangePage = (
-  //     event: React.MouseEvent<HTMLButtonElement> | null,
-  //     newPage: number,
-  //   ) => {
-  //     setPage(newPage);
-  //   };
-
-  //   const handleChangeRowsPerPage = (
-  //     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  //   ) => {
-  //     setRowsPerPage(parseInt(event.target.value, 10));
-  //     setPage(0);
-  //   };
-
-
-
-
-
-
   // const inputRef = useRef(null);
   const onButtonClick = () => {
     inputRef.current.value = "";
@@ -242,24 +227,6 @@ function SheetData() {
     setExcelData(rows);
   };
 
-
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-
-
-  // const currentTableData = useMemo(() =>{
-  //   const firstpage = (currentPage - 1) * PageSize;
-  //   const lastpage = firstpage + PageSize;
-
-  //    if(firstpage === '' || lastpage == ''){
-  //     setExcelData(loading)
-  //    }else{
-  //     setLoading.slice(firstpage,lastpage)
-  //    }
-  //     
-  //    //return excelData.slice(firstpage , lastpage); 
-
-  // },[currentPage])
 
   return (
 
@@ -291,18 +258,6 @@ function SheetData() {
 
       <hr></hr>
       <div style={{ justifyContent: 'space-between' }}>
-        {/* <CustomTablePagination
-          //rowsPerPageOptions={[10, 50, 150, 250, { label: 'All', value: -1 }]}
-          // colSpan={3}
-          rowsPerPage={rowsPerPage}
-          // slotProps={{
-          //   // select: {
-          //   //   'aria-label': 'rows per page',
-          //   // },
-
-          // }}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        /> */}
         <input
           style={{ top: "0", left: "0", padding: '15px 35px', fontSize: '15px' }}
           type='text'
@@ -316,25 +271,33 @@ function SheetData() {
         {excelData !== null && (
 
           <div>
+            {/* <DataGrid
+        pageSize={pageSize}
+        onPageSizeChange={handlePageSizeChange}
+        rowsPerPageOptions={[5, 10, 20]}
+        pagination
+        {...data}
+      /> */}
+
             <table
               className="table"
               style={{ padding: '2% 10% 10% 15%' }}>
               <thead style={{ border: 'rgb(228, 247, 244)', justifyContent: 'space-between', backgroundColor: 'rgb(228, 247, 244)' }} className="table table-striped table-dark">
                 <tr>
 
-                  <th scope="row"><button style={{ border: 'transparent', color: 'white'}}>ID</button></th>
+                  <th scope="row"><button style={{ border: 'transparent', color: 'white' }}>ID</button></th>
                   <th scope="row"><button
                     onClick={() => sorting("Name")} style={{ border: 'transparent', color: 'white' }}>Name</button></th>
                   <th scope="row"><button
                     // onClick={()=> sorting('Email')}
-                    style={{ border: 'transparent', color: 'white'}}>Email</button></th>
+                    style={{ border: 'transparent', color: 'white' }}>Email</button></th>
                   <th scope="row"><button
                     // onClick={()=> sorting('Mobile')}
                     style={{ border: 'transparent', color: 'white' }}>Mobile</button></th>
                   <th scope="row"><button
-                    onClick={() => sorting('Address')} style={{ border: 'transparent', color: 'white'}}>Address</button></th>
+                    onClick={() => sorting('Address')} style={{ border: 'transparent', color: 'white' }}>Address</button></th>
                   <th scope="row"><button
-                    onClick={() => sorting('Country')} style={{ border: 'transparent', color: 'white'}}>Country</button></th>
+                    onClick={() => sorting('Country')} style={{ border: 'transparent', color: 'white' }}>Country</button></th>
                   <th scope="row"><button style={{ border: 'transparent', color: 'white' }}>Status</button></th>
                   <th scope="row"><button style={{ border: 'transparent', color: 'white', marginLeft: '5%' }}>Error</button></th>
 
@@ -346,8 +309,8 @@ function SheetData() {
                   ((rowsPerPage > 0
                     ? excelData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                     : excelData
-                  ))*/}
-                {excelData .slice(pagesVisited, pagesVisited + usersPerPage).map((items, index) => (
+                  )) */}
+                {excelData.slice(pagesVisited, pagesVisited + usersPerPage).map((items, index) => (
                   <tr key={index} style={{ border: 'rgb(243, 212, 212)', backgroundColor: 'rgb(243, 212, 212)', width: '500px' }}>
 
                     <td>{index + 1}</td>
@@ -365,14 +328,10 @@ function SheetData() {
                   </tr>
 
                 ))}
-                {/* {emptyRows > 0 && (
-                  <tr style={{ height: 41 * emptyRows }}>
-                    <td colSpan={3} />
-                  </tr>
-                )} */}
+
               </tbody>
 
-{/* 
+              {/* 
               <CustomTablePagination
                 //  rowsPerPageOptions={[10, 50, 150, 250, { label: 'All', value: -1 }]}
                 // colSpan={3}
@@ -386,18 +345,23 @@ function SheetData() {
  */}
 
             </table>
+            <div style = {{display:'flex'}}>
+            <h4 style={{marginLeft : '5%'}}> 1 - {pagehandler} of {excelData.length}</h4>
             <ReactPaginate
-                previousLabel={"Previous"}
-                nextLabel={"Next"}
-                pageCount={pageCount}
-                onPageChange={changePage}
-                containerClassName={"paginationBttns"}
-                previousLinkClassName={"previousBttn"}
-                nextLinkClassName={"nextBttn"}
-                disabledClassName={"paginationDisabled"}
-                activeClassName={"paginationActive"}
-              />
-        
+              previousLabel={"Previous"}
+              nextLabel={"Next"}
+              pageCount={pageCount}
+              onPageChange={changePage}
+              containerClassName={"paginationBttns"}
+              previousLinkClassName={"previousBttn"}
+              nextLinkClassName={"nextBttn"}
+              disabledClassName={"paginationDisabled"}
+              activeClassName={"paginationActive"}
+              
+            />
+            </div>
+
+
           </div>
         )}
       </div>
